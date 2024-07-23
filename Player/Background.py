2b0a4a44c7032a6e308fd823b0deb7c5b4eaa39f
Munya <cmunene@gsumail.gram.edu>
2024-07-23 05:03:19 +0000

    Update file Background.py

--- Player/Background.py
+++
@@ -1,8 +0,0 @@
-import pygame
-pygame.init()
-prozor = pygame.display.set_mode((200, 200))
-prozor.fill(pygame.Color("white"))
-pygame.draw.rect(prozor, pygame.Color("black"), (20, 20, 160, 160), 5)
-pygame.display.update()
-pygame.time.wait(1800)
-pygame.quit()
