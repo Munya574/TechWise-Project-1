9b32b992de5c0245b3f9b61461bf6f039b13b028
vipstockbauer <vipstockbauer@gmail.com>
2024-08-02 22:39:40 -0500

    Updated Movements

---
+++ Features/Controls.py
@@ -0,0 +1,52 @@
+import pygame
+from pygame.locals import *
+from Tank import Player
+
+class InputManager:
+    def __init__(self, player: Player) -> None:
+        self.running = True
+        self.player = player
+        self.x = 400
+        self.y = 300
+        self.direction = None
+
+    def run(self, keys):
+        while self.running:
+
+            if keys[pygame.K_UP]:
+                self.player.rect.y -= 5
+            if keys[pygame.K_DOWN]:
+                self.player.rect.y += 5
+            if keys[pygame.K_LEFT]:
+                self.player.rect.x -= 5
+            if keys[pygame.K_RIGHT]:
+                self.player.rect.x += 5
+            if keys[pygame.K_SPACE]:
+                if not self.space_pressed:
+                    self.shoot()
+                    self.space_pressed = True
+                else:
+                    self.space_pressed = False
+            if keys[QUIT]:
+                self.running = False
+                    
+
+    def move_up(self):
+        self.direction = "up"
+        self.y -= 5  
+        self.update_player_position()
+        
+    def move_down(self):
+        self.direction = "down"
+        self.y += 5
+        self.update_player_position()
+
+    def move_left(self):
+        self.direction = "left"
+        self.x -= 5
+        self.update_player_position()
+
+    def move_right(self):
+        self.direction = "right"
+        self.x += 5
+        self.update_player_position()
