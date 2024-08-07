9a67f354cf2837902a179d7c3cfa793bc32df935
vipstockbauer <vipstockbauer@gmail.com>
2024-08-06 22:10:11 -0500

    Applied the recommended changes to Controls

--- Features/Controls.py
+++ Features/Controls.py
@@ -7,41 +7,43 @@ class InputManager:
     def __init__(self, player: Player) -> None:
         self.running = True
         self.player = player
+        self.bullets = bullets
         self.x = 400
         self.y = 300
         self.direction = None
 
-pos = pygame.mouse.get_pos()
 
-x_dist = pos[0] - x
-y_dist = -(pos[1]-y)
-angle = math.degrees(math.atan2(y_dist, x_dist))
+def rotation(self):
+    pos = pygame.mouse.get_pos()
 
-turret = pygame.transform.rotate(self.player, angle - 90)
-turret_rect = turret.get_rect(center = (x, y))
+    x_dist = pos[0] - self.rect.centerx
+    y_dist = -(pos[1]-self.rect.centery)
+    angle = math.degrees(math.atan2(y_dist, x_dist))
 
-screen.blit(turret, turret_rect)
+    turret = pygame.transform.rotate(self.player, angle - 90)
+    turret_rect = turret.get_rect(center = (self.rect.centerx, self.rect.centerx))
 
-def run(self, keys):
-        while self.running:
+    self.screen.blit(turret, turret_rect)
 
-            if keys[pygame.K_UP]:
-                self.player.rect.y -= 5
-            if keys[pygame.K_DOWN]:
-                self.player.rect.y += 5
-            if keys[pygame.K_LEFT]:
-                self.player.rect.x -= 5
-            if keys[pygame.K_RIGHT]:
-                self.player.rect.x += 5
-            if keys[pygame.K_SPACE]:
-                if not self.space_pressed:
-                    self.shoot()
-                    self.space_pressed = True
-                else:
-                    self.space_pressed = False
-            if keys[QUIT]:
-                self.running = False
+def input(self, keys):
+    if keys[pygame.K_UP]:
+        self.player.rect.y -= 5
+    if keys[pygame.K_DOWN]:
+        self.player.rect.y += 5
+    if keys[pygame.K_LEFT]:
+        self.player.rect.x -= 5
+    if keys[pygame.K_RIGHT]:
+        self.player.rect.x += 5
+    if keys[pygame.K_SPACE]:
+        if not self.space_pressed:
+            self.shoot()
+            self.space_pressed = True
+    else:
+        self.space_pressed = False
+    if keys[QUIT]:
+        self.running = False
                     
+
 
     # def move_up(self):
     #     self.direction = "up"
