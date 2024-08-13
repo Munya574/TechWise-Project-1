b84d76497a5ab0b98bcc19ff5b00b5e67959ef9d
vipstockbauer <vipstockbauer@gmail.com>
2024-08-12 22:19:09 -0500

    Adjusted image movement to face the correct direction of movement

--- Features/Enemy.py
+++ Features/Enemy.py
@@ -3,40 +3,41 @@ import random
 
 class Enemy:
     def __init__(self):
-        self.original_image = pygame.image.load('enemy.png')
-        self.size = (70, 50)
+        self.original_image = pygame.image.load('enemy.png') 
+        self.size = (70, 50) 
         self.original_image = pygame.transform.scale(self.original_image, self.size)
         
-        self.image = self.original_image
+        self.image = self.original_image  
         self.rect = self.image.get_rect()
-        
-        self.rect.x = random.randint(0, 800 - self.rect.width)  # Random x position
-        self.rect.y = 0
+
+        # Start the enemy at a random position along the top of the screen
+        self.rect.x = random.randint(0, 800 - self.rect.width) 
+        self.rect.y = 0 
 
         self.move_count = 0
-        self.angle = 180  # 0 degrees (facing right)
+        self.angle = 0
         self.clock = pygame.time.Clock()
 
     def draw(self, screen):
         screen.blit(self.image, self.rect)
 
     def update(self):
-        self.clock.tick(80)  
-        
+        self.clock.tick(80)
+
         if self.move_count > 0:
             self.move_count -= 1
             if self.angle == 0:
-                self.rect.x += 2
-            elif self.angle == 90:
-                self.rect.y -= 2
-            elif self.angle == 180:
-                self.rect.x -= 2
-            elif self.angle == 270:
                 self.rect.y += 2
+            elif self.angle == 90:
+                self.rect.x -= 2
+            elif self.angle == 180:
+                self.rect.y -= 2
+            elif self.angle == 270:
+                self.rect.x += 2
         else:
             self.choose_new_direction()
 
-#Boundary checks
+        # Boundary checks
         if self.rect.left < 0:
             self.rect.left = 0
             self.move_count = 0
@@ -54,17 +55,23 @@ class Enemy:
             self.move_count = 0
 
     def choose_new_direction(self):
-        self.move_count = 20  
-        self.angle = random.randint(0, 3) * 90
-        self.rotate_image()
+        self.move_count = random.randint(20, 60) 
+        new_angle = random.choice([0, 90, 180, 270]) 
+        self.rotate_image(new_angle)
+        self.angle = new_angle
 
-    def rotate_image(self):
-        # Rotate the image to face the direction of movement
-        self.image = pygame.transform.rotate(self.original_image, self.angle)
-        # Update the rect after rotation to keep the position correct
+    def rotate_image(self, new_angle):
+        if new_angle == 0:
+            self.image = self.original_image
+        elif new_angle == 270:
+            self.image = pygame.transform.rotate(self.original_image, 90)
+        elif new_angle == 180:
+            self.image = pygame.transform.rotate(self.original_image, 180)
+        elif new_angle == 90:
+            self.image = pygame.transform.rotate(self.original_image, -90)
+
         old_center = self.rect.center
         self.rect = self.image.get_rect(center=old_center)
 
     def collidelist(self):
-
-        return -1    
+        return -1  
