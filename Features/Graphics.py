702576d939aef1c353923d1076448dbea07ee859
moorkan <moorkan4774@gmail.com>
2024-08-16 20:45:26 -0400

    Graphics I added obstacles here to test, we can later add it to a different file

--- Features/Graphics.py
+++ Features/Graphics.py
@@ -1,135 +1,47 @@
+# Obstacles + graphics
 import pygame
 from random import randint
-pygame.init()
 
-WIDTH, HEIGHT = 800, 600
-FPS = 60
+
 TILE = 32
-
-window = pygame.display.set_mode((WIDTH, HEIGHT))
-clock = pygame.time.Clock()
-
-fontUI = pygame.font.Font(None, 30)
+WIDTH, HEIGHT = 800, 600
+window = pygame.display.set_mode((WIDTH, HEIGHT)) 
 
 imgBrick = pygame.image.load('images/bricks.png')
-imgTank = [
-    pygame.image.load('images/pic.png')
-    ]
-imgBangs = [
-    pygame.image.load('images/bang1.png'),
-    pygame.image.load('images/bang2.png'),
-    pygame.image.load('images/bang3.png'),
-    ]
 
 
-
-class UI:
-    def __init__(self):
-        pass
-
-    def update(self):
-        pass
-
-    def draw(self):
-        i = 0
-        for obj in objects:
-            if obj.type == 'tank':
-                pygame.draw.rect(window, obj.color, (5 + i * 70, 5, 22, 22))
-
-                text = fontUI.render(str(obj.rank), 1, 'black')
-                rect = text.get_rect(center = (5 + i * 70 + 11, 5 + 11))
-                window.blit(text, rect)
-
-                text = fontUI.render(str(obj.hp), 1, obj.color)
-                rect = text.get_rect(center = (5 + i * 70 + 32, 5 + 11))
-                window.blit(text, rect)
-                i += 1
-                
-
-    def update(self):  
-        for obj in objects:
-            if obj != self and obj.type == 'block' and self.rect.colliderect(obj.rect):
-                self.rect.topleft = oldX, oldY
-
-        if keys[self.keySHOT] and self.shotTimer == 0:
-            dx = DIRECTS[self.direct][0] * self.bulletSpeed
-            dy = DIRECTS[self.direct][1] * self.bulletSpeed
-            Bullet(self, self.rect.centerx, self.rect.centery, dx, dy, self.bulletDamage)
-            self.shotTimer = self.shotDelay
-
-        if self.shotTimer > 0: self.shotTimer -= 1
-
-    def draw(self):
-        window.blit(self.image, self.rect)
-
-    def damage(self, value):
-        self.hp -= value
-        if self.hp <= 0:
-            objects.remove(self)
-            print(self.color, 'dead')
-
-class Bullet:
-    def __init__(self, parent, px, py, dx, dy, damage):
-        bullets.append(self)
-        self.parent = parent
-        self.px, self.py = px, py
-        self.dx, self.dy = dx, dy
-        self.damage = damage
-
-    def update(self):
-        self.px += self.dx
-        self.py += self.dy
-
-        if self.px < 0 or self.px > WIDTH or self.py < 0 or self.py > HEIGHT:
-            bullets.remove(self)
-        else:
-            for obj in objects:
-                if obj != self.parent and obj.type != 'bang' and obj.type != 'bonus':
-                    if obj.rect.collidepoint(self.px, self.py):
-                        obj.damage(self.damage)
-                        bullets.remove(self)
-                        Bang(self.px, self.py)
-                        break
-
-    def draw(self):
-        pygame.draw.circle(window, 'yellow', (self.px, self.py), 2)
-
-
-class Bang:
-    def __init__(self, px, py):
-        objects.append(self)
-        self.type = 'bang'
-
-        self.px, self.py = px, py
-        self.frame = 0
-
-    def update(self):
-        self.frame += 0.2
-        if self.frame >= 3: objects.remove(self)
-
-    def draw(self):
-        image = imgBangs[int(self.frame)]
-        rect = image.get_rect(center = (self.px, self.py))
-        window.blit(image, rect)
-    
 class Block:
     def __init__(self, px, py, size):
         objects.append(self)
-        self.type = 'block'
-
-        self.rect = pygame.Rect(px, py, size, size)
+        self.type = "tank"
+        
+        self.rect = pygame.Rect (px, py, size, size)
         self.hp = 1
-
-    def update(self):
+        
+    def update (self):
         pass
-
-    def draw(self):
+    
+    def draw (self):
         window.blit(imgBrick, self.rect)
+       # pygame.draw.rect(window, "green", self.rect)
+       # pygame.draw.rect(window, "gray20", self.rect, 2)
+    
+    def damage (self, value):
+       self.hp -= value
+       if self.hp <= 0: objects.pygame.sprite.remove(self)
+       
+objects = []
 
-    def damage(self, value):
-        self.hp -= value
-        if self.hp <= 0: objects.remove(self)
 
-  
-pygame.quit()
-     
+for _ in range (50):
+    while True:
+        x = randint(0, WIDTH // TILE -1) *TILE
+        y = randint(0, HEIGHT // TILE -1) *TILE
+        rect = pygame.Rect(x, y, TILE, TILE) 
+        fined = False
+        for obj in objects:
+            if rect.colliderect(obj.rect): fined =True
+            
+        if not fined: break
+    Block(x, y, TILE) 
+    
